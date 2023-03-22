da132a04c07b734cf6aabb1ed89c2eb05b87ea39
Mcc <1033570101@qq.com>
2023-03-22 14:49:30 +0800

    Update combineFile.sh

--- combineFile.sh
+++ combineFile.sh
@@ -8,4 +8,4 @@ cd ${root}/
 cd ${fitsPath}
 ls *.fits > fitslist.txt
 ./combine_fits ${root}/${sourceName} fitslist.txt 
-./update_Header-OFFSSUB ${sourceName}.fits
+#./update_Header-OFFSSUB ${sourceName}.fits
